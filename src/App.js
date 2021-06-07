e0851a2c5320de2c51663bcb46ec89291d2076c7
firas <firas.shtayyeh@gmail.com>
2021-06-07 13:49:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ya Firas alkbeeer.
+          Edit <code>src/App.js</code> and save to reload ya Firas alkbeeer travis.
         </p>
         <a
           className="App-link"
